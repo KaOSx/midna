99a03a1d05dae9b3a2346bf7c28089f51009d68a
fabianalexisinostroza <fabian.alexis@openmailbox.org>
2016-03-04 10:14:45 -0300

    midna light icons

--- icons/midna/mimetypes/small/linkmod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PATTERN1=`echo "$2"`
-PATTERN2=`echo "$3"`
-LINKNAME=`echo "$1"`
-OLDTARGET=`readlink "$1"`
-NEWTARGET=`echo "$OLDTARGET" \
-| sed -e 's/'"$PATTERN1"'/'"$PATTERN2"'/'`
-echo ln -nsf "$NEWTARGET" "$LINKNAME"
